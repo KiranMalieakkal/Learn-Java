2aac673bd98d22590c42fb73e1f0a066402957ef
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-04-14 20:10:50 +0200

    adding hello athul to hello.java

--- ZArrays/Hello.java
+++ ZArrays/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello subin");
         System.out.println("hello Kiran");
+        System.out.println("hello Athul");
 
     }
     
